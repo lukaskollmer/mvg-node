f0769dab2286be7869534765fb950f7b2dc8882b
Lukas Kollmer <lukas.kollmer@gmail.com>
2017-07-25 14:34:15 +0200

    update cli

--- cli.js
+++ cli.js
@@ -45,7 +45,7 @@ HasZoomData: ${station.hasZoomData}`;
       const station = await mvg.getStation(cli.input[1]);
       let departures = await mvg.getDepartures(station);
 
-      console.log(`Upcoming departures for '${station.name}'`);
+      console.log(`Upcoming departures for '${station.name}':`);
 
       const table = new Table({
         head: ['  ⏳', ''],
