cdc209e5be3e55fc55e7fcf03e98002d75262cc5
Lukas Kollmer <lukas.kollmer@gmail.com>
2019-04-04 12:20:14 +0200

    refactor cli

--- cli.js
+++ cli.js
@@ -24,11 +24,10 @@ const cli = meow(`
 	  $ mvg-node departures 'Hauptbahnhof'
 `);
 
-(async () => {
-  switch (cli.input[0]) {
-    case 'station': {
-      const station = await mvg.getStation(cli.input[1]);
-      const output =
+
+const showStationInfo = async () => {
+  const station = await mvg.getStation(cli.input[1]);
+  const output =
 `Name: ${station.name}
 ID: ${station.id}
 Place: ${station.place}
@@ -38,46 +37,59 @@ Longitude: ${station.longitude}
 HasLiveData: ${station.hasLiveData}
 HasZoomData: ${station.hasZoomData}`;
 
-      console.log(output);
+  console.log(output);
+};
+
+
+
+const showDepartures = async () => {
+  const station = await mvg.getStation(cli.input[1]);
+  let departures = await mvg.getDepartures(station);
+  
+  console.log('');
+  console.log(`Upcoming departures for '${station.name}':`);
+  
+  const table = new Table({
+    head: ['  ⏳', ''],
+    colWidths: [8, 40]
+  });
+  
+  if (!cli.flags.all) {
+    departures = departures.slice(0, 10);
+  }
+  
+  const type = {
+    BUS: 'Bus ',
+    REGIONAL_BUS: 'Bus ',
+    SBAHN: '',
+    UBAHN: '',
+    TRAM: 'Tram '
+  };
+  let now = moment();
+  
+  departures.forEach(d => {
+    let duration = Math.ceil(moment.duration(moment(d.time).diff(now)).asMinutes());
+    
+    if (duration < 5) {
+      duration = chalk.red(`${duration} min`);
+    } else {
+      duration = chalk.green(`${duration} min`);
+    }
+    
+    table.push([`${duration}`, `${type[d.product]}${d.label} ${d.destination}`]);
+  });
+  console.log(table.toString());
+};
+
+
+(async () => {
+  switch (cli.input[0]) {
+    case 'station': {
+      await showStationInfo();
       break;
     }
     case 'departures': {
-      const station = await mvg.getStation(cli.input[1]);
-      let departures = await mvg.getDepartures(station);
-
-      console.log('');
-      console.log(`Upcoming departures for '${station.name}':`);
-
-      const table = new Table({
-        head: ['  ⏳', ''],
-        colWidths: [8, 40]
-      });
-
-      if (!cli.flags.all) {
-        departures = departures.slice(0, 10);
-      }
-
-      const type = {
-        BUS: 'Bus ',
-        REGIONAL_BUS: 'Bus ',
-        SBAHN: '',
-        UBAHN: '',
-        TRAM: 'Tram '
-      };
-      let now = moment();
-
-      departures.forEach(d => {
-        let duration = Math.ceil(moment.duration(moment(d.time).diff(now)).asMinutes());
-
-        if (duration < 5) {
-          duration = chalk.red(`${duration} min`);
-        } else {
-          duration = chalk.green(`${duration} min`);
-        }
-
-        table.push([`${duration}`, `${type[d.product]}${d.label} ${d.destination}`]);
-      });
-      console.log(table.toString());
+      await showDepartures();
       break;
     }
     default: break;
